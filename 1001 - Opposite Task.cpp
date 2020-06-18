53368e2e1a36e66062ee316a7002c9c8a92be042
Tahsin Amin <Tahsin16@cse.pstu.ac.bd>
2020-06-18 13:57:40 +0600

    Create 	1001 - Opposite Task.cpp

---
+++ 1001 - Opposite Task.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll ;
+typedef double dou;
+typedef long long int lli;
+#define pi acos(-1.0)
+ 
+ 
+int main(){
+ll t;
+cin>>t;
+ 
+while(t--){
+    ll n;
+    cin>>n;
+    ll x = n/2;
+    ll last = n-x;
+    cout<<x<<" "<<(n-x)<<endl;
+}
+}
+ 
